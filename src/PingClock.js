e34cb2d3ca48d384d7b9da6fc3e85f64ea3c3153
Meersalzeis <kaifrichter@gmail.com>
2022-06-12 22:50:52 +0200

    basically initial commit

---
+++ src/PingClock.js
@@ -0,0 +1,18 @@
+import { useEffect } from 'react'
+
+/*
+function TimerComponent() {
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("Has started")
+    }, 100);
+
+    setInterval(() => {
+      console.log('1 sec. passed');
+    }, 1000);
+  }, []);
+}
+*/
+
+//export { TimerComponent }
